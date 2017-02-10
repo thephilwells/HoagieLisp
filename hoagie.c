732adac5acbbe4471a6cc4c0bd1c5ebdc1325689
Phil Wells <philwells@thephilwells.com>
2017-02-10 07:19:43 -0500

    add language parser

--- hoagie.c
+++ hoagie.c
@@ -28,6 +28,21 @@ void add_history(char* unused) {}
 #endif
 
 int main(int argc, char** argv) {
+    // Create some parsers
+    mpc_parser_t* Number    = mpc_new("number");
+    mpc_parser_t* Operator  = mpc_new("operator");
+    mpc_parser_t* Expr      = mpc_new("expr");
+    mpc_parser_t* Hoagie    = mpc_new("hoagie");
+
+    // Define them with the following language
+    mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                     \
+     number     : /-?[0-9]+/ ;                            \
+     operator   : '+' | '-' | '*' | '/' ;                 \
+     expr       : <number> | '(' <operator> <expr>+ ')' ; \
+     hoagie     : /^/ <operator> <expr>+ /$/ ;
+    ",
+    Number, Operator, Expr, Hoagie);
 
     /* Print Version and Exit information */
     puts("\nHoagieLisp Version 0.0.0.1");
@@ -43,6 +58,7 @@ int main(int argc, char** argv) {
         printf("You typed: %s", input);
         free(input);
     }
-
+    // Undefine and delete our parsers
+    mpc_cleanup(4, Number, Operator, Expr, Hoagie);
     return 0;
 }
