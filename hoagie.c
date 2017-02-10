a89e7e234cc1e7d1b3c00089409d8a1cf42e59c4
Phil Wells <phil.wells@kaplan.com>
2017-02-10 16:14:21 -0500

    evaluates basic arithmetic (lispy)

--- hoagie.c
+++ hoagie.c
@@ -27,6 +27,9 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #endif
 
+long eval (mpc_ast_t*);
+long eval_op (long, char*, long);
+
 int main(int argc, char** argv) {
     // Create some parsers
     mpc_parser_t* Number    = mpc_new("number");
@@ -59,7 +62,8 @@ int main(int argc, char** argv) {
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Hoagie, &r)) {
             // On success print the AST
-            mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else {
             // Otherwise print the error
@@ -70,5 +74,36 @@ int main(int argc, char** argv) {
     }
     // Undefine and delete our parsers
     mpc_cleanup(4, Number, Operator, Expr, Hoagie);
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+    // If tagged as number, return it directly
+    if(strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    // The operator is always the second child
+    char* op = t->children[1]->contents;
+
+    // We store the third child in 'x'
+    long x = eval(t->children[2]);
+
+    // Iterate the remaining children, and combine results
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+    return x;
+}
+
+// Use operator string to see which operation to perform
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    if (strcmp(op, "%") == 0) { return x % y; }
     return 0;
 }
